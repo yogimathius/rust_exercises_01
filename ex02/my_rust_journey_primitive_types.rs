0b1d0ce4072fc052a84c6ea741621acd19fa0666
Mathius <info@yogimathius.dev>
2024-03-29 21:43:24 -0700

    fix: add types to ints

--- ex02/my_rust_journey_primitive_types.rs
+++ ex02/my_rust_journey_primitive_types.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let uinteger_value_400 = 402;
-    let integer_value_14 = -986;
-    let uinteger_value_guess = 1030;
+    let uinteger_value_400: u32 = 400;
+    let integer_value_14: i32 = 14;
+    let uinteger_value_guess: u32 = 0x2f;
     let true_boolean = true;
     let false_boolean = false;
     let float_variable = 3.14;
